31374921b66290c957afc26e6bf81b69b6b4bb56
AlekhyaKattamuri06 <alekhya.kattamuri@slu.edu>
2024-10-20 12:41:31 -0500

    Changes in the backend

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,13 @@
+// models/User.js
 const mongoose = require('mongoose');
 
-// Define the User schema
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  username: { type: String, required: true },  // Ensure this field is added
-  phone: { type: String, required: true },     // Ensure this field is added
-  address: { type: String, required: true },   // Ensure this field is added
+  password: { type: String, required: true }
 });
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
