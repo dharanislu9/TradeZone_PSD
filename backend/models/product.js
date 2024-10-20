e507936e62fad0e40b0a0a4394d260f2124eb836
dharanislu9 <dharani.nomula@slu.edu>
2024-10-19 22:35:44 -0500

    adding product.js and updating server.js - s2

---
+++ backend/models/product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  worth: { type: Number, default: 50 },
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
