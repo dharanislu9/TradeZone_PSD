70cdbf6f33e92d29c50cc6f61b08b990583ba429
dharanislu9 <dharani.nomula@slu.edu>
2024-10-19 22:55:24 -0500

    updating app.js with routing - s2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import ProductDetails from './components/ProductDetails';
 import Register from './components/Register';
 import Login from './components/Login';
+import ForgotPassword from './components/ForgotPassword'; 
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // State for login status
+
+  const handleLogout = () => {
+    setIsLoggedIn(false); // Update login status
+  };
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        {/* <Route path="/Login" element={<Login />} /> */}
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<HomePage handleLogout={handleLogout} />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Forgot password route */}
       </Routes>
     </Router>
   );
