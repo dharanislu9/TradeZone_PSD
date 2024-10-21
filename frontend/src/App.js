2a68a94cf9ee3b4407f80849427748cb9d2511e6
Saikiran0412 <chsaikiran0412@gmail.com>
2024-10-20 23:48:14 -0500

    Updating App file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage'
 import HomePage from './components/HomePage';
 import Register from './components/Register';
 import Login from './components/Login';
+import SellerPage from './components/SellerPage';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LandingPage />} />
+        <Route path="/HomePage" element={<HomePage />} />
         <Route path="/Register" element={<Register />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/SellerPage" element={<SellerPage/>} />
+
       </Routes>
     </Router>
   );
